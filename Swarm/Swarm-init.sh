8bc94f8f937a173a360e1b7a118a0c5e3c209ce0
Makrov_an <makrov_an@icloud.com>
2025-01-17 10:18:29 +0800

    swarm mode added

---
+++ Swarm/Swarm-init.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -x
+
+if [ $(id -u) = 0 ]; then
+    echo "Need root!"
+    exit 1
+fi
+
+# https://docs.docker.com/engine/swarm/stack-deploy/
+
+# Start nfs directory:
+apt update
+apt install -y nfs-kernel-server
+# # on clients: 
+# sudo apt install nfs-common
+mkdir /share
+# ln -s /home/user/Projects/Hadoop-in-Docker/Data /share
+# Grant full access to all users: 
+echo "/share *(rw,async,no_subtree_check,no_root_squash)">>/etc/exports
+exportfs -a
+# sudo mount $(hostname):/share/Data /Data
+
+# Start docker registry:
+systemctl stop docker
+echo '{"insecure-registries":["192.168.1.119:5000"]}' > /etc/docker/daemon.json
+systemctl start docker
+docker run -d -p 5000:5000 --name registry registry:2
+# curl -X GET http://192.168.1.119:5000/v2/_catalog
+
+# Start swarm:
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+docker swarm init --advertise-addr $LOCAL_IP
+docker node update --label-add TAG=manager $(hostname)
+echo "Enter worker-node name:"
+read WORKER_1
+docker node update --label-add TAG=worker-1 $WORKER_1
+
+docker compose up -d
+# wait for images:
+sleep 1000
+docker compose down
+docker compose push
+
+docker stack deploy --compose-file docker-compose.yml -d=false hadoopstack
+
+# docker service ls
+# docker node inspect $(hostname) | grep TAG
+# docker node inspect $WORKER_1 | grep TAG
+# docker exec -it <container-name-or-id> bash
+# docker stack rm hadoopstack
+# docker rm $(docker ps -a -q)
+# docker rmi $(docker image ls -a -q)
+# docker swarm leave --force
+# docker system prune
+exit 0
